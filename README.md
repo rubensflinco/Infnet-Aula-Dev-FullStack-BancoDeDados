8235b5143a6215780e560025a5ad012c2df71f6a
Rubens Flinco <contato@rubensflinco.com.br>
2023-06-26 18:31:21 -0300

    up

---
+++ README.md
@@ -0,0 +1,18 @@
+# Infnet-Aula-Dev-FullStack-BancoDeDados
+
+## Depencias:
+- NodeJS
+- NPM
+- Instalar das bibliotecas do NPM use o comando ```npm install``` tanto na pasta do front quando no back antes de rodar pela primeira vez os projetos.
+
+## Rodando:
+- Para iniciar use o comando ```npm run dev```.
+- Acesse o endereço http://localhost:4000/usuario.
+- Pronto.
+
+## Links:
+- https://desktop.github.com/
+- https://code.visualstudio.com/download
+- https://git-scm.com/downloads
+- https://nodejs.org/en
+- https://create-react-app.dev/docs/getting-started/
---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const mysql = require('mysql');
+
+// Configurações da conexão com o banco de dados
+const dbConfig = {
+    host: 'rubensflinco.com.br',
+    user: 'rubensflinco_express',
+    password: 'uvXGWTbmvKwz',
+    database: 'rubensflinco_infnet'
+};
+
+// Criação da conexão com o banco de dados
+const connection = mysql.createConnection(dbConfig);
+
+// Criação do aplicativo Express
+const app = express();
+
+// Rota para recuperar todos os usuários
+app.get('/usuario', (req, res) => {
+
+    // Reconecta-se ao banco de dados antes de executar a consulta SQL
+    connection.connect(function(err) {
+        if (err) {
+            console.error('Erro ao conectar-se ao banco de dados: ' + err.stack);
+            return;
+        }
+        console.log('Conexão bem sucedida com o ID: ' + connection.threadId);
+    });
+
+    const query = 'SELECT * FROM usuario';
+
+    // Executa a consulta SQL
+    connection.query(query, (error, results) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send('Erro ao recuperar usuários');
+        } else {
+            res.json(results);
+        }
+    });
+});
+
+// Inicia o servidor
+app.listen(4000, () => {
+    console.log('Servidor iniciado na porta 4000');
+});
